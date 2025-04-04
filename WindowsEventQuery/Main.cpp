17f7d3e716c1bcaed98004ec32f3da9378adb795
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-04-04 10:51:41 +0200

    .

--- WindowsEventQuery/Main.cpp
+++ WindowsEventQuery/Main.cpp
@@ -4,17 +4,19 @@
 
 
 int wmain() {
-    std::vector<std::wstring> channels = EnumerateChannels();
-
+#if 0
     wprintf(L"List of Channels\n\n");
+    std::vector<std::wstring> channels = EnumerateChannels();
     for (const std::wstring& channel : channels) {
         wprintf(L"  %s\n", channel.c_str());
 
         //PrintChannelProperties(channel);
         //wprintf(L"\n");
     }
+#endif
 
     // perform event query
+    std::wstring channel = L"System";
     std::wstring query = L"Event/System[EventID=7023]";
     EventQuery(L"System", query);
 
