e8a858f82e23f6055bdf5340a8364ffbfac32c1d
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-04-06 11:43:18 +0200

    .

--- WindowsEventQuery/Publisher.cpp
+++ WindowsEventQuery/Publisher.cpp
@@ -3,6 +3,28 @@
 #include <cassert>
 
 
+// Contains the value and message string for a type, such as
+// an opcode or task that the provider defines or uses. If the
+// type does not specify a message string, the message member
+// contains the value of the type's name attribute.
+typedef struct _msgstring {
+    union {
+        DWORD dwValue;  // Value attribute for opcode, task, and level
+        UINT64 ullMask; // Mask attribute for keyword
+    };
+    LPWSTR pwcsMessage; // Message string or name attribute
+} MSG_STRING, * PMSG_STRING;
+
+// Header for the block of value/message string pairs. The dwSize member
+// is the size, in bytes, of the block of MSG_STRING structures to which 
+// the pMessage member points.
+typedef struct _messages {
+    DWORD dwSize;
+    PMSG_STRING pMessage;
+} MESSAGES, * PMESSAGES;
+
+
+
 Event GetPublisherMetadata(std::wstring publisherId) {
     return EvtOpenPublisherMetadata(NULL, publisherId.c_str(), NULL, /*locale*/0, 0);
 }
