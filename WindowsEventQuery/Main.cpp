4968a62156d7b66b9593e1ca82fb42bbf1d0a019
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-04-06 17:20:03 +0200

    Change query to filter on severity level.

--- WindowsEventQuery/Main.cpp
+++ WindowsEventQuery/Main.cpp
@@ -25,7 +25,7 @@ int wmain() {
 #if 1
     // perform event query
     std::wstring channel = L"System";
-    std::wstring query = L"*/System[EventID=7023]";
+    std::wstring query = L"*/System[(Level>=1) and (Level<=2)]";
     std::wstring publisherName = L"";
     EventQuery(channel, query, publisherName, 20);
 #endif
