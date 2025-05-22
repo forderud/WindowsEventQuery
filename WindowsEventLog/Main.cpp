c523ec33c29193cae61809001f7d5747ecbc750d
Fredrik Orderud <forderud@gmail.com>
2025-05-22 22:30:00 +0200

    .

--- WindowsEventLog/Main.cpp
+++ WindowsEventLog/Main.cpp
@@ -49,12 +49,24 @@ int main() {
     EventLog log(L"MyEventProvider"); // or L"Application" or L"System"
 
     // DOC: https://learn.microsoft.com/en-us/windows/win32/eventlog/reporting-an-event
-    WORD type     = EVENTLOG_WARNING_TYPE; // or EVENTLOG_INFORMATION_TYPE or EVENTLOG_SUCCESS or other EVENTLOG_xxx types
-    WORD category = NETWORK_CATEGORY; // source-specific category
-    DWORD eventId = MSG_RETRIES; // entry in the message file associated with the event source
-    const wchar_t* messages[] = { L"25", L"zero"};
+    {
+        WORD type = EVENTLOG_WARNING_TYPE; // or EVENTLOG_INFORMATION_TYPE or EVENTLOG_SUCCESS or other EVENTLOG_xxx types
+        WORD category = NETWORK_CATEGORY; // source-specific category
+        DWORD eventId = MSG_RETRIES; // entry in the message file associated with the event source
+        const wchar_t* messages[] = { L"25", L"zero" };
 
-    wprintf(L"Writing log entry...\n");
-    log.WriteLog(type, category, eventId, std::size(messages), messages);
-    printf("[done]\n");
+        wprintf(L"Writing log entry...\n");
+        log.WriteLog(type, category, eventId, std::size(messages), messages);
+        printf("[done]\n");
+    }
+    {
+        WORD type = EVENTLOG_INFORMATION_TYPE; // or EVENTLOG_INFORMATION_TYPE or EVENTLOG_SUCCESS or other EVENTLOG_xxx types
+        WORD category = UI_CATEGORY; // source-specific category
+        DWORD eventId = MSG_COMPUTE_CONVERSION; // entry in the message file associated with the event source
+        const wchar_t* messages[] = { L"8", L"2" };
+
+        wprintf(L"Writing log entry...\n");
+        log.WriteLog(type, category, eventId, std::size(messages), messages);
+        printf("[done]\n");
+    }
 }
