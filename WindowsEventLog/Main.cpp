ddaeb4ecdfbd0174e26383ea0a80d36fcba309fc
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-05-22 14:09:06 +0200

    .

--- WindowsEventLog/Main.cpp
+++ WindowsEventLog/Main.cpp
@@ -1,21 +1,24 @@
 #include <Windows.h>
 #include <Shlobj.h> // for IsUserAnAdmin
+#include <comdef.h>
 #include <cassert>
 #include <iostream>
 
 
 int main() {
+#if 0
     if (!IsUserAnAdmin()) {
         wprintf(L"ERROR: Admin privileges missing.\n");
         return -1;
     }
+#endif
 
     // open log
-    const wchar_t LOG_NAME[] = L"System";
+    const wchar_t LOG_NAME[] = L"Application"; // L"System"
     HANDLE h = RegisterEventSourceW(NULL, LOG_NAME);
     if (!h) {
-        DWORD err = GetLastError();
-        wprintf(L"ERROR: RegisterEventSourceW failed with %u\n", err);
+        _com_error err(GetLastError());
+        wprintf(L"ERROR: RegisterEventSourceW failed (%s)\n", err.ErrorMessage());
         abort();
     }
 
@@ -23,12 +26,11 @@ int main() {
     wprintf(L"Adding a '%s' log entry to the %s log..\n", message, LOG_NAME);
     BOOL ok = ReportEventW(h, EVENTLOG_SUCCESS, 0, 0, NULL, 1, 0, &message, NULL);
     if (!ok) {
-        DWORD err = GetLastError();
-        wprintf(L"ERROR: ReportEventW failed with %u\n", err);
+        _com_error err(GetLastError());
+        wprintf(L"ERROR: ReportEventW failed (%s)\n", err.ErrorMessage());
         abort();
     }
     printf("[done]\n");
 
     DeregisterEventSource(h);
-
 }
