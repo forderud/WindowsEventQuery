5cbdc5c1ae6d80864dbf99f0acdabe23a0afc9f6
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-05-22 17:14:16 +0200

    Add initial self-registration impl.

--- MyEventProvider/dllmain.cpp
+++ MyEventProvider/dllmain.cpp
@@ -1,5 +1,7 @@
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 #include <windows.h>
+#include <atlbase.h>
+#include <cassert>
 
 
 /** DLL entry point. */
@@ -17,11 +19,44 @@ BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpRese
 
 // Adds entries to the system registry
 STDAPI DllRegisterServer() {
+    CRegKey parent;
+    LSTATUS res = parent.Open(HKEY_LOCAL_MACHINE, L"SYSTEM\\CurrentControlSet\\Services\\EventLog\\Application", KEY_READ | KEY_WRITE);
+    assert(res == ERROR_SUCCESS);
+
+    CRegKey reg;
+    res = reg.Create(parent, L"MyEventProvider");
+    assert(res == ERROR_SUCCESS);
+
+    {
+        res = reg.SetDWORDValue(L"CategoryCount", 3);
+        assert(res == ERROR_SUCCESS);
+
+        res = reg.SetStringValue(L"CategoryMessageFile", L"C:\\Dev\\WindowsEventQuery\\x64\\Debug\\MyEventProvider.dll");
+        assert(res == ERROR_SUCCESS);
+
+        res = reg.SetStringValue(L"EventMessageFile", L"C:\\Dev\\WindowsEventQuery\\x64\\Debug\\MyEventProvider.dll");
+        assert(res == ERROR_SUCCESS);
+
+        res = reg.SetStringValue(L"ParameterMessageFile", L"C:\\Dev\\WindowsEventQuery\\x64\\Debug\\MyEventProvider.dll");
+        assert(res == ERROR_SUCCESS);
+
+
+        res = reg.SetDWORDValue(L"TypesSupported", 7);
+        assert(res == ERROR_SUCCESS);
+    }
+
     return NOERROR;
 }
 
 
 // Removes entries from the system registry
 STDAPI DllUnregisterServer() {
+    CRegKey parent;
+    LSTATUS res = parent.Open(HKEY_LOCAL_MACHINE, L"SYSTEM\\CurrentControlSet\\Services\\EventLog\\Application", KEY_READ | KEY_WRITE);
+    assert(res == ERROR_SUCCESS);
+
+    res = parent.DeleteSubKey(L"MyEventProvider");
+    assert(res == ERROR_SUCCESS);
+
     return NOERROR;
 }
