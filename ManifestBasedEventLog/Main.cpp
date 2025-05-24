2c575e7b84a55165e244d5e44859436c7fb34e00
Fredrik Orderud <forderud@gmail.com>
2025-05-24 21:13:36 +0200

    .

--- ManifestBasedEventLog/Main.cpp
+++ ManifestBasedEventLog/Main.cpp
@@ -20,7 +20,6 @@ enum TRANSFER_TYPE {
 };
 
 #define MAX_NAMEDVALUES          5  // Maximum array size
-#define MAX_PAYLOAD_DESCRIPTORS  9 + (2 * MAX_NAMEDVALUES)
 
 struct NAMEDVALUE {
     LPCWSTR name;
@@ -74,33 +73,30 @@ int wmain(void) {
     EventHandle provider(&PROVIDER_GUID);
 
     // Data to load into event descriptors
-    USHORT Scores[3] = { 45, 63, 21 };
-    void* pImage = &Scores;
-    DWORD TransferType = Upload;
-    DWORD Day = MONDAY | TUESDAY;
-    LPCWSTR Path = L"c:\\path\\folder\\file.ext";
-    BYTE Cert[11] = { 0x2, 0x4, 0x8, 0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x0, 0x1 };
-    PBYTE Guid = (PBYTE)&PROVIDER_GUID;
-    USHORT ArraySize = MAX_NAMEDVALUES;
-    BOOL IsLocal = TRUE;
-    NAMEDVALUE NamedValues[MAX_NAMEDVALUES] = {
-        {L"Bill", 1},
-        {L"Bob", 2},
-        {L"William", 3},
-        {L"Robert", 4},
-        {L"", 5}
-    };
 
     // Load the array of data descriptors for the TransferEvent event. 
     // Add the data to the array in the order of the <data> elements
     // defined in the event's template. 
     std::vector<EVENT_DATA_DESCRIPTOR> parameters;
+
+    USHORT Scores[3] = { 45, 63, 21 };
+    void* pImage = &Scores;
     parameters.push_back(EventDataArg(&pImage, sizeof(pImage)));
     parameters.push_back(EventDataArg(Scores, sizeof(Scores)));
+
+    PBYTE Guid = (PBYTE)&PROVIDER_GUID;
     parameters.push_back(EventDataArg(Guid, sizeof(GUID)));
+
+    BYTE Cert[11] = { 0x2, 0x4, 0x8, 0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x0, 0x1 };
     parameters.push_back(EventDataArg(Cert, sizeof(Cert)));
+
+    BOOL IsLocal = TRUE;
     parameters.push_back(EventDataArg(&IsLocal, sizeof(BOOL)));
+
+    LPCWSTR Path = L"c:\\path\\folder\\file.ext";
     parameters.push_back(EventDataArg(Path, (ULONG)(wcslen(Path) + 1) * sizeof(WCHAR)));
+
+    USHORT ArraySize = MAX_NAMEDVALUES;
     parameters.push_back(EventDataArg(&ArraySize, sizeof(USHORT)));
 
     // If your event contains a structure, you should write each member
@@ -113,12 +109,23 @@ int wmain(void) {
     //
     // Because the array of structures in this example contains both strings 
     // and numbers, you must write each member of the structure separately.
+    NAMEDVALUE NamedValues[MAX_NAMEDVALUES] = {
+        {L"Bill", 1},
+        {L"Bob", 2},
+        {L"William", 3},
+        {L"Robert", 4},
+        {L"", 5}
+    };
+
     for (int j = 0; j < MAX_NAMEDVALUES; j++) {
         parameters.push_back(EventDataArg(NamedValues[j].name, (ULONG)(wcslen(NamedValues[j].name) + 1) * sizeof(WCHAR)));
         parameters.push_back(EventDataArg(&(NamedValues[j].value), sizeof(USHORT)));
     }
 
+    DWORD Day = MONDAY | TUESDAY;
     parameters.push_back(EventDataArg(&Day, sizeof(DWORD)));
+
+    DWORD TransferType = Upload;
     parameters.push_back(EventDataArg(&TransferType, sizeof(DWORD)));
 
     // Write the event. You do not have to verify if your provider is enabled before
