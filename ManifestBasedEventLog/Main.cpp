bfc30375d677e5018e23056b6b50834823d52fd6
Fredrik Orderud <forderud@gmail.com>
2025-05-24 21:43:52 +0200

    .

--- ManifestBasedEventLog/Main.cpp
+++ ManifestBasedEventLog/Main.cpp
@@ -65,38 +65,34 @@ static EVENT_DATA_DESCRIPTOR EventDataArg(const void* DataPtr, ULONG DataSize) {
 int wmain(void) {
     EventHandle provider(&PROVIDER_GUID);
 
-    // Data to load into event descriptors
-
-    // Load the array of data descriptors for the TransferEvent event. 
-    // Add the data to the array in the order of the <data> elements
-    // defined in the event's template. 
+    // Prepare UserData parameters for the TransferEvent event. 
+    // Parameter order need to match the EventData element order.
     std::vector<EVENT_DATA_DESCRIPTOR> parameters;
 
-    USHORT Scores[3] = { 45, 63, 21 };
-    void* pImage = &Scores;
+    USHORT scores[3] = { 45, 63, 21 };
+    void* pImage = &scores;
     parameters.push_back(EventDataArg(&pImage, sizeof(pImage)));
-    parameters.push_back(EventDataArg(Scores, sizeof(Scores)));
+    parameters.push_back(EventDataArg(scores, sizeof(scores)));
 
     parameters.push_back(EventDataArg(&PROVIDER_GUID, sizeof(PROVIDER_GUID)));
 
-    BYTE Cert[11] = { 0x2, 0x4, 0x8, 0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x0, 0x1 };
-    parameters.push_back(EventDataArg(Cert, sizeof(Cert)));
+    BYTE cert[11] = { 0x2, 0x4, 0x8, 0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x0, 0x1 };
+    parameters.push_back(EventDataArg(cert, sizeof(cert)));
 
-    BOOL IsLocal = TRUE;
-    parameters.push_back(EventDataArg(&IsLocal, sizeof(IsLocal)));
+    BOOL isLocal = TRUE;
+    parameters.push_back(EventDataArg(&isLocal, sizeof(isLocal)));
 
     std::wstring Path = L"c:\\path\\folder\\file.ext";
     parameters.push_back(EventDataArg(Path.c_str(), (ULONG)(Path.length()+1)*sizeof(wchar_t)));
 
-    DWORD Day = MONDAY | TUESDAY;
-    parameters.push_back(EventDataArg(&Day, sizeof(Day)));
+    DWORD days = MONDAY | TUESDAY;
+    parameters.push_back(EventDataArg(&days, sizeof(days)));
 
-    DWORD TransferType = Upload;
-    parameters.push_back(EventDataArg(&TransferType, sizeof(TransferType)));
+    DWORD transferType = Upload;
+    parameters.push_back(EventDataArg(&transferType, sizeof(transferType)));
 
     // Write the event. You do not have to verify if your provider is enabled before
     // writing the event. ETW will write the event to any session that enabled
-    // the provider. If no session enabled the provider, the event is not 
-    // written.
+    // the provider. If no session enabled the provider, the event is not written.
     provider.Write(&TransferEvent, (ULONG)parameters.size(), parameters.data());
 }
