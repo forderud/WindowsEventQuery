d4b9b24005df958128e35e4d96fa349c219a0e1b
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-05-25 20:36:10 +0200

    Add extra "WarningMessage" log type.

--- MyEventLogger/Main.cpp
+++ MyEventLogger/Main.cpp
@@ -7,31 +7,42 @@
 int wmain(void) {
     EventHandle provider(&PROVIDER_GUID);
 
-    // Prepare UserData parameters for the TransferEvent event. 
-    // Parameter order need to match the EventData element order.
-    std::vector<EVENT_DATA_DESCRIPTOR> parameters;
+    {
+        std::vector<EVENT_DATA_DESCRIPTOR> parameters;
+        std::wstring path = L"Something strange happened...";
+        parameters.push_back(EventDataArg(path.c_str(), (ULONG)(path.length() + 1) * sizeof(wchar_t))); // incl. null-termination
 
-    USHORT scores[3] = { 45, 63, 21 };
-    parameters.push_back(EventDataArg(scores, sizeof(scores)));
+        wprintf(L"Writing log entry...\n");
+        provider.Write(&WarningMessage, (ULONG)parameters.size(), parameters.data());
+    }
 
-    parameters.push_back(EventDataArg(&PROVIDER_GUID, sizeof(PROVIDER_GUID)));
+    {
+        // Prepare UserData parameters for the TransferEvent event. 
+        // Parameter order need to match the EventData element order.
+        std::vector<EVENT_DATA_DESCRIPTOR> parameters;
 
-    BYTE cert[11] = { 0x2, 0x4, 0x8, 0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x0, 0x1 };
-    parameters.push_back(EventDataArg(cert, sizeof(cert)));
+        USHORT scores[3] = { 45, 63, 21 };
+        parameters.push_back(EventDataArg(scores, sizeof(scores)));
 
-    std::wstring path = L"C:\\SomeFolder\\SomeFile.txt";
-    parameters.push_back(EventDataArg(path.c_str(), (ULONG)(path.length()+1)*sizeof(wchar_t))); // incl. null-termination
+        parameters.push_back(EventDataArg(&PROVIDER_GUID, sizeof(PROVIDER_GUID)));
 
-    DWORD day = 3;
-    parameters.push_back(EventDataArg(&day, sizeof(day)));
+        BYTE cert[11] = { 0x2, 0x4, 0x8, 0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x0, 0x1 };
+        parameters.push_back(EventDataArg(cert, sizeof(cert)));
 
-    DWORD transfer = 99;
-    parameters.push_back(EventDataArg(&transfer, sizeof(transfer)));
+        std::wstring path = L"C:\\SomeFolder\\SomeFile.txt";
+        parameters.push_back(EventDataArg(path.c_str(), (ULONG)(path.length() + 1) * sizeof(wchar_t))); // incl. null-termination
 
-    // Write the event. You do not have to verify if your provider is enabled before
-    // writing the event. ETW will write the event to any session that enabled
-    // the provider. If no session enabled the provider, the event is not written.
-    wprintf(L"Writing log entry...\n");
-    provider.Write(&TransferEvent, (ULONG)parameters.size(), parameters.data());
+        DWORD day = 3;
+        parameters.push_back(EventDataArg(&day, sizeof(day)));
+
+        DWORD transfer = 99;
+        parameters.push_back(EventDataArg(&transfer, sizeof(transfer)));
+
+        // Write the event. You do not have to verify if your provider is enabled before
+        // writing the event. ETW will write the event to any session that enabled
+        // the provider. If no session enabled the provider, the event is not written.
+        wprintf(L"Writing log entry...\n");
+        provider.Write(&TransferEvent, (ULONG)parameters.size(), parameters.data());
+    }
     wprintf(L"[done]\n");
 }
