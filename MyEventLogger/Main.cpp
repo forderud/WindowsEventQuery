b10d3ad87177b6da75f010b61e5d35aa8fd05aed
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-05-25 20:37:50 +0200

    .

--- MyEventLogger/Main.cpp
+++ MyEventLogger/Main.cpp
@@ -8,41 +8,41 @@ int wmain(void) {
     EventHandle provider(&PROVIDER_GUID);
 
     {
-        std::vector<EVENT_DATA_DESCRIPTOR> parameters;
+        std::vector<EVENT_DATA_DESCRIPTOR> params;
         std::wstring path = L"Something strange happened...";
-        parameters.push_back(EventDataArg(path.c_str(), (ULONG)(path.length() + 1) * sizeof(wchar_t))); // incl. null-termination
+        params.push_back(EventDataArg(path.c_str(), (ULONG)(path.length() + 1) * sizeof(wchar_t))); // incl. null-termination
 
         wprintf(L"Writing log entry...\n");
-        provider.Write(&WarningMessage, (ULONG)parameters.size(), parameters.data());
+        provider.Write(&WarningMessage, (ULONG)params.size(), params.data());
     }
 
     {
         // Prepare UserData parameters for the TransferEvent event. 
         // Parameter order need to match the EventData element order.
-        std::vector<EVENT_DATA_DESCRIPTOR> parameters;
+        std::vector<EVENT_DATA_DESCRIPTOR> params;
 
         USHORT scores[3] = { 45, 63, 21 };
-        parameters.push_back(EventDataArg(scores, sizeof(scores)));
+        params.push_back(EventDataArg(scores, sizeof(scores)));
 
-        parameters.push_back(EventDataArg(&PROVIDER_GUID, sizeof(PROVIDER_GUID)));
+        params.push_back(EventDataArg(&PROVIDER_GUID, sizeof(PROVIDER_GUID)));
 
         BYTE cert[11] = { 0x2, 0x4, 0x8, 0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x0, 0x1 };
-        parameters.push_back(EventDataArg(cert, sizeof(cert)));
+        params.push_back(EventDataArg(cert, sizeof(cert)));
 
         std::wstring path = L"C:\\SomeFolder\\SomeFile.txt";
-        parameters.push_back(EventDataArg(path.c_str(), (ULONG)(path.length() + 1) * sizeof(wchar_t))); // incl. null-termination
+        params.push_back(EventDataArg(path.c_str(), (ULONG)(path.length() + 1) * sizeof(wchar_t))); // incl. null-termination
 
         DWORD day = 3;
-        parameters.push_back(EventDataArg(&day, sizeof(day)));
+        params.push_back(EventDataArg(&day, sizeof(day)));
 
         DWORD transfer = 99;
-        parameters.push_back(EventDataArg(&transfer, sizeof(transfer)));
+        params.push_back(EventDataArg(&transfer, sizeof(transfer)));
 
         // Write the event. You do not have to verify if your provider is enabled before
         // writing the event. ETW will write the event to any session that enabled
         // the provider. If no session enabled the provider, the event is not written.
         wprintf(L"Writing log entry...\n");
-        provider.Write(&TransferEvent, (ULONG)parameters.size(), parameters.data());
+        provider.Write(&TransferEvent, (ULONG)params.size(), params.data());
     }
     wprintf(L"[done]\n");
 }
