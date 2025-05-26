234168369a2f4e42b61eaf278454bd17c3469959
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-05-26 19:22:17 +0200

    Add a C# sample project.

---
+++ MyEventLoggerCs/Program.cs
@@ -0,0 +1,13 @@
+﻿using MyEvent;
+
+namespace MyEventLoggerCs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var log = new MyEventProvider();
+            log.EventWriteInteractiveInfoEvent("Hello log!");
+        }
+    }
+}
