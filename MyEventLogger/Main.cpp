4e8d69a1ed436f34740900e85484a11c39c28ae4
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-05-27 13:09:17 +0200

    Delete old events to clean up.

--- MyEventLogger/Main.cpp
+++ MyEventLogger/Main.cpp
@@ -8,33 +8,23 @@ int wmain(void) {
     EventHandle provider(&MyMoviePlayer);
 
     {
-        // generate WarningMessage log entry
+        // generate CacheSizeMB log entry
         std::vector<EVENT_DATA_DESCRIPTOR> params;
-        DWORD val = 41;
+        DWORD val = 512;
         params.push_back(EventData(&val, sizeof(val)));
 
         wprintf(L"Writing log entry...\n");
-        provider.Write(&WarningMessage, params.size(), params.data());
+        provider.Write(&CacheSizeMB, params.size(), params.data());
     }
 
     {
-        // generate ErrorMessage log entry
+        // generate Playback log entry
         std::vector<EVENT_DATA_DESCRIPTOR> params;
-        std::wstring path = L"Something fucked up...";
+        std::wstring path = L"Avatar";
         params.push_back(EventData(path.c_str(), (path.length() + 1) * sizeof(wchar_t))); // incl. null-termination
 
         wprintf(L"Writing log entry...\n");
-        provider.Write(&ErrorMessage, params.size(), params.data());
-    }
-
-    {
-        // generate InteractiveInfoEvent log entry
-        std::vector<EVENT_DATA_DESCRIPTOR> params;
-        std::wstring path = L"User clicked on X...";
-        params.push_back(EventData(path.c_str(), (path.length() + 1) * sizeof(wchar_t))); // incl. null-termination
-
-        wprintf(L"Writing log entry...\n");
-        provider.Write(&InteractiveInfoEvent, params.size(), params.data());
+        provider.Write(&Play, params.size(), params.data());
     }
 
     wprintf(L"[done]\n");
